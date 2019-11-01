80b5841f62927ae9a8312b1438557a59339d7eec
duckladydinh <duckladydinh@gmail.com>
2019-11-01 10:55:16 +0100

    Create arc091F.py

---
+++ arc091F.py
@@ -0,0 +1,14 @@
+def main():
+    n = int(input())
+    res = 0
+    for (a, k) in [map(int, input().split()) for _ in range(n)]:
+        while a > k and a % k != 0:
+            m = a // k
+            x = (a - k * m - 1) // (m + 1)
+            a -= max(1, x) * (m + 1)
+        res ^= a // k
+    print('Takahashi' if res != 0 else 'Aoki')
+
+
+if __name__ == '__main__':
+    main()
